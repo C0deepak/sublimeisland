c8679ebef6013e04a6a8214256ffbf779b63961e
Deepak <deepakkumarkasyap12@gmail.com>
2024-04-22 18:15:33 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,112 +1,113 @@
-import Image from "next/image";
+"use client"
+import Navbar from "@/components/Navbar";
+import { AlarmSmoke, ChevronRight, HandPlatter, Mail, MapPin, ParkingCircle, Phone, Star, Wifi } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen">
+      <Navbar />
+      <div className="flex flex-col gap-4 px-8">
+        <div className="flex items-center gap-2 py-2 border-b-2"><span className="text-slate-500">Hotels </span><ChevronRight size={18} /> Andaman Castle</div>
+      </div>
+
+      <div className="flex p-8 gap-8">
+        <div className="w-[30%]  flex flex-col rounded-md shadow-md overflow-hidden">
+          <img src="/img/image.webp" alt="" className="w-full" />
+          <div className="flex justify-between p-4 border-b-2">
+            <div className="flex flex-col">
+              <p className="font-semibold">Andaman Castel</p>
+              <span className="flex gap-1 text-slate-400 items-center text-sm"><MapPin size={16} />South Point, Shadipur, Port Blair</span>
+            </div>
+            <div className="bg-yellow-400 text-white flex items-center gap-1 px-2 py-1 h-fit text-sm font-bold rounded-md"><Star size={16} /> 4.5</div>
+          </div>
+
+          <div className="flex justify-between p-4 border-b-2">
+            <div className="flex flex-col">
+              <p className="font-semibold">Description</p>
+              <span className="flex gap-1 text-slate-400 items-center text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non ut minus quasi eum iusto debitis ad reiciendis iste porro nihil velit blanditiis sapiente ratione, fugit soluta asperiores. Modi consequuntur quos repudiandae laboriosam repellat sapiente quibusdam veniam culpa nostrum odit.
+              </span>
+            </div>
+          </div>
+
+          <div className="flex flex-col p-4 gap-2">
+            <div className="flex justify-between">
+              <div className="w-1/2 flex items-center gap-2 text-sm">
+                <div className="bg-slate-200 w-8 h-8 rounded-full flex items-center justify-center"><Wifi size={14} /></div>
+                <p>Free wifi</p>
+              </div>
+              <div className="w-1/2 flex items-center gap-2 text-sm">
+                <div className="bg-slate-200 w-8 h-8 rounded-full flex items-center justify-center"><HandPlatter size={14} /></div>
+                <p>Room Service</p>
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div className="w-1/2 flex items-center gap-2 text-sm">
+                <div className="bg-slate-200 w-8 h-8 rounded-full flex items-center justify-center"><ParkingCircle size={14} /></div>
+                <p>Car Parking</p>
+              </div>
+              <div className="w-1/2 flex items-center gap-2 text-sm">
+                <div className="bg-slate-200 w-8 h-8 rounded-full flex items-center justify-center"><AlarmSmoke size={14} /></div>
+                <p>No Smoking</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="w-[70%] flex flex-col gap-4">
+          <div className="flex flex-col gap-4 rounded-md shadow-md p-4">
+            <p className="font-semibold uppercase">Feel free to Reach Us</p>
+            <div className="flex justify-between">
+              <div className="w-1/2 flex items-center gap-2 text-sm">
+                <div className="bg-emerald-100 text-emerald-600 w-8 h-8 rounded-full flex items-center justify-center"><Phone size={14} /></div>
+                <p>+91 8096453324</p>
+              </div>
+              <div className="w-1/2 flex items-center gap-2 text-sm">
+                <div className="bg-emerald-100 text-emerald-600 w-8 h-8 rounded-full flex items-center justify-center"><Mail size={14} /></div>
+                <p>travelisland@gmail.com</p>
+              </div>
+            </div>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="flex flex-col gap-4 rounded-md shadow-md p-4">
+            <p className="font-semibold uppercase">Please Fill up the contact form</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <form action='https://formspree.io/f/mayradge' method='POST'>
+              <div class="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                  <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="John" required />
+                </div>
+                <div>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                  <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="john@gmail.com" required />
+                </div>
+                <div>
+                  <label for="phone" class="block mb-2 text-sm font-medium text-gray-900">Phone</label>
+                  <input type="tel" id="phone" name="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="0987654321" pattern="[0-9]{10}" required />
+                </div>
+                <div>
+                  <label for="date" class="block mb-2 text-sm font-medium text-gray-900">Date</label>
+                  <input type="text" id="date" name="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="Apr 22, 2024-Apr 23, 2024" required />
+                </div>
+                <div>
+                  <label for="people" class="block mb-2 text-sm font-medium text-gray-900">No. of Peoples</label>
+                  <input type="number" id="people" name="peoples" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="2" required />
+                </div>
+                <div>
+                  <label for="rooms" class="block mb-2 text-sm font-medium text-gray-900">No. of Rooms</label>
+                  <input type="rooms" id="visitors" name="rooms" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="1" required />
+                </div>
+              </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              <div class="mb-6">
+                <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Message (Optional)</label>
+                <textarea rows={4} cols={2} id="message" name="message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" placeholder="Hi! I want to ask, ..." />
+              </div>
+              <button type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+            </form>
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='py-4 px-8'>
+            <img src="/img/silogo.png" alt="" className='w-36'/>
+        </div>
+    )
+}
+
+export default Navbar
